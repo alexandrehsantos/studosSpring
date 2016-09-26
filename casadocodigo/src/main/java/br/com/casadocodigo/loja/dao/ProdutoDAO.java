38247084481608a78fcf049eaf21a0a1f0962b4a
alexandre <alexandrehsantos@gmail.com>
2016-09-25 23:55:48 -0300

    Dao e outras interacoes

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/dao/ProdutoDAO.java
@@ -0,0 +1,11 @@
+package br.com.casadocodigo.loja.dao;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public class ProdutoDAO {
+	
+}
